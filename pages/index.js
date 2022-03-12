3e58ea0733a27ddea1e1da102276c5a568514d33
Klemen Pavlovič <klemenpavlovic@gmail.com>
2022-03-12 20:57:15 +0100

    Add example link

--- pages/index.js
+++ pages/index.js
@@ -2,6 +2,14 @@ import React from "react";
 
 // This file represents the root page of our app
 
+import Link from "next/link";
+
 export default function Home() {
-  return <div>Ok</div>;
+  return (
+    <div>
+      <Link href={{ pathname: "/[username]", query: { username: "john123" } }}>
+        <a>John&apos;s profile</a>
+      </Link>
+    </div>
+  );
 }
