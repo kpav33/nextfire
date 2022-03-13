2537d9521791fb1bf22fb5da3b04e64cc247b484
Klemen Pavlovič <klemenpavlovic@gmail.com>
2022-03-13 14:25:43 +0100

    Add Google Signin with firebase

--- pages/enter.js
+++ pages/enter.js
@@ -2,10 +2,50 @@ import React from "react";
 
 // Check the component tree with React Dev Tools and you can see that Next automatically adds a bunch of components to the component tree that we don't directly see in our code
 
+import { auth, googleAuthProvider } from "../lib/firebase";
+
 export default function EnterPage() {
+  const user = null;
+  const username = null;
+
+  // 1. user signed out <SignInButton />
+  // 2. user signed in, but missing username <UsernameForm />
+  // 3. user signed in, has username <SignOutButton />
   return (
     <main>
-      <h1>Sign Up</h1>
+      {user ? (
+        !username ? (
+          <UsernameForm />
+        ) : (
+          <SignOutButton />
+        )
+      ) : (
+        <SignInButton />
+      )}
     </main>
   );
+}
+
+// Sign in with Google button
+function SignInButton() {
+  const signInWithGoogle = async () => {
+    // Triggers a pop up modal, where user can sign in with their Google account
+    // This could be wrapped in a try/catch block
+    await auth.signInWithPopup(googleAuthProvider);
+  };
+
+  return (
+    <button className="btn-google" onClick={signInWithGoogle}>
+      <img src={"/google.png"} /> Sign in with Google
+    </button>
+  );
+}
+
+// Sign out button
+function SignOutButton() {
+  return <button onClick={() => auth.signOut()}>Sign Out</button>;
+}
+
+function UsernameForm() {
+  return null;
 }
