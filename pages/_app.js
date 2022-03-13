4f12e6ba340c23e26e72793cd2a2612dbb551a64
Klemen Pavlovič <klemenpavlovic@gmail.com>
2022-03-13 15:17:49 +0100

    Add auth hook

--- pages/_app.js
+++ pages/_app.js
@@ -4,14 +4,18 @@ import "../styles/globals.css";
 // You can use it to add UI components that are available on every page, like a Navbar or Footer or use it for managing authentication state on the front end
 
 import { UserContext } from "../lib/context";
+import { useUserData } from "../lib/hooks";
 import Navbar from "../components/Navbar";
 import { Toaster } from "react-hot-toast";
 
 function MyApp({ Component, pageProps }) {
+  const userData = useUserData();
+  console.log(userData);
+
   return (
     // By wrapping the app with the Provider we will give all components access to the Context values
     // We can overwrite the default context values
-    <UserContext.Provider value={{ user: {}, username: "jeff" }}>
+    <UserContext.Provider value={userData}>
       <Navbar />
       <Component {...pageProps} />
       <Toaster />
