42796a4480d6c0986940f39ab3dbb5ae7b446afb
Klemen Pavlovič <klemenpavlovic@gmail.com>
2022-03-13 13:53:28 +0100

    Add navbar component

--- components/Navbar.js
+++ components/Navbar.js
@@ -1,5 +1,44 @@
 import React from "react";
+import Link from "next/link";
 
 export default function Navbar() {
-  return <div>Navbar</div>;
+  const user = null;
+  const username = null;
+
+  return (
+    <nav className="navbar">
+      <ul>
+        <li>
+          <Link href="/" passHref>
+            <button className="btn-logo">FEED</button>
+          </Link>
+        </li>
+
+        {/* user is signed-in and has username */}
+        {username && (
+          <>
+            <li className="push-left">
+              <Link href="/admin" passHref>
+                <button className="btn-blue">Write Posts</button>
+              </Link>
+            </li>
+            <li>
+              <Link href={`/${username}`} passHref>
+                <img src={user?.photoURL} />
+              </Link>
+            </li>
+          </>
+        )}
+
+        {/* user is not signed OR has not created username */}
+        {!username && (
+          <li>
+            <Link href="/enter" passHref>
+              <button className="btn-blue">Log in</button>
+            </Link>
+          </li>
+        )}
+      </ul>
+    </nav>
+  );
 }
--- pages/_app.js
+++ pages/_app.js
@@ -3,8 +3,15 @@ import "../styles/globals.css";
 // _app.js is a main entry point for any page in the application, it's like a wrapper or layout that surrounds other pages
 // You can use it to add UI components that are available on every page, like a Navbar or Footer or use it for managing authentication state on the front end
 
+import Navbar from "../components/Navbar";
+
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Navbar />
+      <Component {...pageProps} />
+    </>
+  );
 }
 
 export default MyApp;
