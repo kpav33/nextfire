5e2ec6d6dc082f1ab6a34e06d641d32b225190ec
Klemen Pavlovič <klemenpavlovic@gmail.com>
2022-03-14 10:50:36 +0100

    Add paginated home page feed

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 
 // This file represents the root page of our app
 // In Next.js file structure in the pages directory, mirrors how your URLs are structured in the browser
@@ -7,10 +7,45 @@ import React from "react";
 import Link from "next/link";
 import Loader from "../components/Loader";
 import toast from "react-hot-toast";
+import PostFeed from "../components/PostFeed";
+import { firestore, fromMillis, postToJSON } from "../lib/firebase";
 
-export default function Home() {
+// Max post to query per page
+const LIMIT = 10;
+
+export default function Home(props) {
+  const [posts, setPosts] = useState(props.posts);
+  const [loading, setLoading] = useState(false);
+  const [postsEnd, setPostsEnd] = useState(false);
+
+  const getMorePosts = async () => {
+    setLoading(true);
+    const last = posts[posts.length - 1];
+
+    const cursor =
+      typeof last.createdAt === "number"
+        ? fromMillis(last.createdAt)
+        : last.createdAt;
+
+    const query = firestore
+      .collectionGroup("posts")
+      .where("published", "==", true)
+      .orderBy("createdAt", "desc")
+      .startAfter(cursor)
+      .limit(LIMIT);
+
+    const newPosts = (await query.get()).docs.map((doc) => doc.data());
+
+    setPosts(posts.concat(newPosts));
+    setLoading(false);
+
+    if (newPosts.length < LIMIT) {
+      setPostsEnd(true);
+    }
+  };
+
   return (
-    <div>
+    <main>
       {/* <Link
         prefetch={false}
         href={{ pathname: "/[username]", query: { username: "john123" } }}
@@ -18,7 +53,30 @@ export default function Home() {
         <a>John&apos;s profile</a>
       </Link>
       <Loader show={false} /> */}
-      <button onClick={() => toast.success("Hello toast!")}>Toast Me!</button>
-    </div>
+      {/* <button onClick={() => toast.success("Hello toast!")}>Toast Me!</button> */}
+      <PostFeed posts={posts} />
+
+      {!loading && !postsEnd && (
+        <button onClick={getMorePosts}>Load more</button>
+      )}
+
+      <Loader show={loading} />
+
+      {postsEnd && "You have reached the end!"}
+    </main>
   );
+}
+
+export async function getServerSideProps(context) {
+  const postsQuery = firestore
+    .collectionGroup("posts")
+    .where("published", "==", true)
+    .orderBy("createdAt", "desc")
+    .limit(LIMIT);
+
+  const posts = (await postsQuery.get()).docs.map(postToJSON);
+
+  return {
+    props: { posts }, // will be passed to the page component as props
+  };
 }
