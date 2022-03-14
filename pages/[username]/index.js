69f279c584ebddf1c8636e9b70babcfa2cc31bb0
Klemen Pavlovič <klemenpavlovic@gmail.com>
2022-03-14 10:26:45 +0100

    Add SSR user profile page

--- pages/[username]/index.js
+++ pages/[username]/index.js
@@ -1,9 +1,41 @@
 import React from "react";
+import { getUserWithUsername, postToJSON } from "../../lib/firebase";
+import UserProfile from "../../components/UserProfile";
+import PostFeed from "../../components/PostFeed";
 
 // You can also apply the square brackets that mark a dynamic routes to directories not just to files
 // In this case this means that any route that we navigate to beyond the route URL will render this component
 // This does not conflict with any other static routes, because Next will give preference to any routes that already exists in a static form versus the dynamic routes that this file represents
 
-export default function UserProfilePage() {
-  return <div>UserProfilePage</div>;
+export default function UserProfilePage({ user, posts }) {
+  return (
+    <main>
+      <UserProfile user={user} />
+      <PostFeed posts={posts} />
+    </main>
+  );
+}
+
+export async function getServerSideProps({ query }) {
+  const { username } = query;
+
+  const userDoc = await getUserWithUsername(username);
+
+  // JSON serializable data
+  let user = null;
+  let posts = null;
+
+  if (userDoc) {
+    user = userDoc.data();
+    const postsQuery = userDoc.ref
+      .collection("posts")
+      .where("published", "==", true)
+      .orderBy("createdAt", "desc")
+      .limit(5);
+    posts = (await postsQuery.get()).docs.map(postToJSON);
+  }
+
+  return {
+    props: { user, posts }, // will be passed to the page component as props
+  };
 }
