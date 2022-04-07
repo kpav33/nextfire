da81ee9684cbe079f1585ee315520deb3b13db88
Klemen Pavlovič <klemenpavlovic@gmail.com>
2022-04-07 12:14:28 +0200

    Add AuthCheck component

--- pages/admin/index.js
+++ pages/admin/index.js
@@ -1,8 +1,13 @@
 import React from "react";
+import AuthCheck from "../../components/AuthCheck";
 
 // To create a basic route you can either create a file with the name of the route or a directory followed by an index.js file
 // This sort of a pages have 1 to 1 relationship, meaning one route points to one component => This are so-called static routes, but sometimes you need dynamic routes
 
-export default function AdminPostsPage() {
-  return <div>AdminPostsPage</div>;
+export default function AdminPostsPage(props) {
+  return (
+    <main>
+      <AuthCheck></AuthCheck>
+    </main>
+  );
 }
